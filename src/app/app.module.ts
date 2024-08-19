9db1281824ad2a924a47fe8a9e1e1403ad007f7e
JesusVoltage <jesusreyesreal@gmail.com>
2024-08-19 16:40:14 +0200

    comit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,47 @@
 import { NgModule } from '@angular/core';
+import { NgOptimizedImage } from '@angular/common'
+import { provideImageKitLoader } from '@angular/common';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAuth, getAuth } from '@angular/fire/auth';
+import { provideDatabase, getDatabase } from '@angular/fire/database';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+import { provideStorage, getStorage } from '@angular/fire/storage';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { FooterComponent } from './footer/footer.component';
+import { ShopComponent } from './shop/shop.component';
+import { AdminComponent } from './admin/admin.component';
+import { ConciertosComponent } from './conciertos/conciertos.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    FooterComponent,
+    ShopComponent,
+    AdminComponent,
+    ConciertosComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    NgOptimizedImage,
+    AppRoutingModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideDatabase(() => getDatabase()),
+    provideFirestore(() => getFirestore()),
+    provideStorage(() => getStorage())
   ],
-  providers: [],
+  providers: [
+    provideImageKitLoader("https://ik.imagekit.io/arungudelli/")
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebase: {
+    projectId: 'garum-d8d05',
+    appId: '1:915580717408:web:32b5f11c701cb6eb10f605',
+    storageBucket: 'garum-d8d05.appspot.com',
+    apiKey: 'AIzaSyAYx8aL5VEJQFiix1RvFkxAy-SPzOe-dT0',
+    authDomain: 'garum-d8d05.firebaseapp.com',
+    messagingSenderId: '915580717408',
+    measurementId: 'G-QEQJKW0WEH',
+  },
+};
